c3602b8531c100c5294f5a8e61d70ba2c6193f74
Gurwan Delaunay <gurwandelaunay35@gmail.com>
2020-12-21 12:57:45 +0100

    add sound alert and notification alert for unix devices

--- kill_the_myopia.py
+++ kill_the_myopia.py
@@ -1,23 +1,47 @@
 import time
+import subprocess
+
+def sendmessage(message):
+    subprocess.Popen(['notify-send', "Kill The Myopia",message])
+    return
 
 def main():
     nbLap = 0
     while True:
-        time.sleep(1200) #1200 secondes -> 20 minutes
+        noPause()
         pause()
         nbLap += 1
         print("You took", nbLap, "breaks")
 
+def noPause():
+    t = 10 #1200 secondes -> 20 minutes
+    while t:
+        min, sec = divmod(t, 60)
+        printformat = '{:02d}:{:02d}'.format(min, sec)
+        print(printformat, end='\r')
+        time.sleep(1)
+        t -= 1
+
+    print('\007')
+    print("-----")
+    print("Break time")
+    print("-----")
+    sendmessage("Break time")
 
 def pause():
-    pauseTime = 300 #300 secondes -> 5 minutes
+    pauseTime = 5 #300 secondes -> 5 minutes
     while pauseTime:
         min, sec = divmod(pauseTime, 60)
         printformat = '{:02d}:{:02d}'.format(min, sec)
         print(printformat, end='\r')
         time.sleep(1)
         pauseTime -= 1
-    print("You can come back !")
+
+    print('\007')
+    print("-----")
+    print("Screen time")
+    print("-----")
+    sendmessage("Screen time")
 
 if __name__ == "__main__":
     print("Welcome in Kill The Myopia !")
